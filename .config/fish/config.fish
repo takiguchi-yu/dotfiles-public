5e8458915b56d9fa72242dfddf08ad65491d67a8
takiguchi-yu <takiguchi-yu@gnavi.co.jp>
2023-01-08 11:26:17 +0900

    feat :sparkles: initial commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,25 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set fish_greeting ""
+
+set -gx TERM xterm-256color
+
+# theme
+set -g theme_color_scheme terminal-dark
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_display_user yes
+set -g theme_hide_hostname no
+set -g theme_hostname always
+
+# aliases
+alias ls "ls -p -G"
+alias la "ls -A"
+alias ll "ls -l"
+alias lla "ll -A"
+alias g git
+
+### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
+set --export --prepend PATH "/Users/takiguchi-yu/.rd/bin"
+### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
