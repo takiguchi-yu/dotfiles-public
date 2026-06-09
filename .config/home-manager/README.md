78c63951223836522840699de6c8ead1ae55454a
takiguchi-yu <8340629+takiguchi-yu@users.noreply.github.com>
2026-06-09 20:07:57 +0900

    chore: :building_construction: update nix

---
+++ .config/home-manager/README.md
@@ -0,0 +1,5 @@
+home manager configuration for my personal use.
+
+```sh
+home-manager switch
+```
--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -4,18 +4,72 @@
   home.username = "takiguchi-yu";
   home.homeDirectory = "/Users/takiguchi-yu";
   home.stateVersion = "25.11";
+  
   home.packages = with pkgs; [
     gh
     ripgrep
+    lefthook
+    mysql84
+    postgresql_18
+    golangci-lint
+    ollama
+    docker
+    docker-compose
+    colima
+    python3
+    pipx
   ];
-  home.file = {
-  };
-  home.sessionVariables = {
-  };
+
+  # シェルに依存しない PATH の追加
+  home.sessionPath = [
+    "$HOME/.local/bin"
+    "$HOME/.orbstack/bin"
+  ];
 
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
+  };
+
+  programs.java = {
+    enable = true;
+    package = pkgs.jdk21;
+  };
+
+  programs.fish = {
+    enable = true;
+
+    # エイリアスの設定
+    shellAliases = {
+      ls = "ls -p -G";
+      la = "ls -A";
+      ll = "ls -l";
+      lla = "ll -A";
+      g = "git";
+      hm = "home-manager";
+      hms = "home-manager switch";
+      nixu = "cd ~/.config/home-manager/ && nix flake update && cd -";
+      claude = "headroom wrap claude";
+      copilot = "headroom wrap copilot";
+    };
+
+    # ASDFの設定とCopilot用関数の直接定義
+    shellInit = ''
+      set fish_greeting ""
+      
+      # ASDF configuration code
+      if test -z $ASDF_DATA_DIR
+          set _asdf_shims "$HOME/.asdf/shims"
+      else
+          set _asdf_shims "$ASDF_DATA_DIR/shims"
+      end
+
+      # Do not use fish_add_path because it potentially changes the order
+      if not contains $_asdf_shims $PATH
+          set -gx --prepend PATH $_asdf_shims
+      end
+      set --erase _asdf_shims
+    '';
   };
 
   programs.home-manager.enable = true;
